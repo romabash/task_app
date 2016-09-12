e4d1a156c8924e329a76f8d962717f6fff9f18a8
Roma <romanofkin@gmail.com>
2016-09-11 18:27:02 -0700

    Updated Clear button with focus on input field

--- js/script.js
+++ js/script.js
@@ -1,33 +1,40 @@
-document.getElementById("inputField").focus();
 
+//Initial focus on the inut box
+var inputField = document.getElementById('inputField');
+inputField.focus();
+
+//Bind Return keyup to Add button
+inputField.addEventListener('keyup', function(event){
+  event.preventDefault();
+  if(event.keyCode == 13) {
+    myButton.click();
+  }
+});
+
+//Set the error message to hidden
 var errorMessage = document.getElementById('error');
 errorMessage.style.visibility = 'hidden';
 
-var clearBtn = document.getElementById('btn1');
-clearBtn.style.visibility = 'hidden';
-
+//Hide function
 function hide (obj){
 	obj.style.visibility = 'hidden';	
 }
 
-clearBtn.onclick = function(){hide(clearBtn)};
-
+//Shows error message and hides after 2 seconds
 function show(){
 	errorMessage.style.visibility = 'visible';
 	setTimeout(function(){hide(errorMessage)}, 2000);
   document.getElementById("inputField").focus();
 }
 
+//Setting Events to the Add button to show error message 
 var myButton = document.getElementById('btn2');
-myButton.onclick = show;
+myButton.addEventListener('click', show);
 
-var inputField = document.getElementById('inputField');
+//Setting focus on input filed after clicking Clear button
+var clearButton = document.getElementById('btn1');
+clearButton.addEventListener('click', function(){inputField.focus();});
 
-inputField.addEventListener('keyup', function(event){
-  event.preventDefault();
-  if(event.keyCode == 13) {
-    myButton.click();
-    clearBtn.style.visibility = 'visible';
-  }
-});
+
+
 
