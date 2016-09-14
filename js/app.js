bcf93b3ac0a1ae404c017753b3bd6265b82a839e
Roma <romanofkin@gmail.com>
2016-09-13 20:19:00 -0700

    Added Local Storage

--- js/app.js
+++ js/app.js
@@ -5,14 +5,23 @@
   var app = angular.module('myApp', []);
   
   app.controller('myCtrl', function($scope){
-    $scope.products = [];
-	  $scope.placeholder = "Enter a task here";
-	
+    
+    $scope.placeholder = "Enter a task here";  
+    //Check to see if Local Storage is supported and assigns products to it or empty array
+    if (typeof(Storage) !== "undefined") {
+      $scope.products = JSON.parse(localStorage.getItem("tasks")) || [];
+    } else {
+      $scope.products = [];
+    }
+
 	  $scope.addItem = function(){
 	    $scope.errorText = "";
 	    if(!$scope.addMe){ return; }
 	    if($scope.products.indexOf($scope.addMe) === -1 ){ //checks index of addMe (-1 if not in array)
         $scope.products.push($scope.addMe);
+        if (typeof(Storage) !== "undefined") {
+          localStorage.setItem("tasks", JSON.stringify($scope.products));
+        } 
 	      $scope.addMe = "";
 	    }
 	    else{
@@ -23,9 +32,15 @@
 	  $scope.removeItem = function(x){
 	    $scope.errorText = "";
 	    $scope.products.splice(x,1);
+      if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("tasks", JSON.stringify($scope.products));
+      } 
 	  }
     $scope.removeAll = function(){
       $scope.products = [];
+      if (typeof(Storage) !== "undefined") {
+        localStorage.setItem("tasks", JSON.stringify($scope.products));
+      } 
     }
   });
   
