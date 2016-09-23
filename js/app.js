43ff83e862942b6fb695a7c34640de2c1bcfaf49
Roma <romanofkin@gmail.com>
2016-09-22 19:40:41 -0700

    Added service with factory with Undo function

--- js/app.js
+++ js/app.js
@@ -2,47 +2,116 @@
 (function(){
   'use strict';
   
-  var app = angular.module('myApp', []);
-  
-  app.controller('myCtrl', function($scope){
+  angular.module('MyApp', [])
+  .controller('TaskCtrl', TaskCtrl) //set up controller with its function
+	.service('StorageService', StorageService)  //set up service with its main function
+	.factory('TaskFactory', TaskFactory);  //set up service with its function
     
-    $scope.placeholder = "Enter a task here";  
-    //Check to see if Local Storage is supported and assigns products to it or empty array
-    if (typeof(Storage) !== "undefined") {
-      $scope.products = JSON.parse(localStorage.getItem("tasks")) || [];
-    } else {
-      $scope.products = [];
-    }
+//Controller
+  TaskCtrl.$inject = ['TaskFactory'];
+  function TaskCtrl(TaskFactory){
+    var list = this;
+	  var taskList = TaskFactory(); //setting taskList to be the returned TaskService
+	  
+	  list.items = taskList.getItems(); //getting items from TaskService
+	  list.addMe = ""; //new task from User input
+    
+    //adds item using TaskService addItem function
+    list.addItem = function(){
+		  try{
+		    list.errorMessage = "";
+		    taskList.addItem(list.addMe);
+		    list.addMe = "";
+		  }
+      catch (error){
+		    list.errorMessage = error.message; //'thrown' error in TaskService
+		    list.addMe = "";
+		  }
+	  };
+    
+    list.removeItem = function (itemIndex){
+		  taskList.removeItem(itemIndex);
+		  list.errorMessage = "";
+		  list.addMe = "";
+	  };
+    
+    list.clear = function(){
+		  taskList.clear();
+		  list.errorMessage = "";
+		  list.addMe = "";
+	  };
+    
+    list.undo = function(){
+		  taskList.undo();
+		  list.items = taskList.getItems(); //reinitialize items 
+	  };
+  }
+  
+//Service - StorageService
+  function StorageService(){
+    var service = this;
+    
+    //loads data from LocalStorage
+    service.load = function(){
+      var load = JSON.parse(localStorage.getItem("tasks")) || [];
+		  return load;
+    };
+    
+    //saves data to LocalStorage
+    service.save = function(items){
+      localStorage.setItem("tasks", JSON.stringify(items));
+    };
+  }
+  
+//Service - TaskService
+  function TaskService(StorageService){
+    var service = this;
+    var items = StorageService.load();
+    var temp = StorageService.load();  //holds temporally LocalStorage data
+    
+    service.addItem = function(task){
+      if(!task){
+        return; //if no input
+      }
+      else if(items.indexOf(task) === -1){
+        items.push(task);
+        StorageService.save(items); //saves new task into LocalStorage
+        temp = StorageService.load(); //assign new LocalStorage data 
+      }
+      else{
+        throw new Error ("Item already in the list"); //throw this error when input same task
+      }
+    };
+    
+    service.removeItem = function(itemIndex){
+      temp = StorageService.load(); //assign LocalStorage data before deleting a task
+	    items.splice(itemIndex,1); 
+		  StorageService.save(items); //after deleting, save to LocalStorage
+    };
+    
+    service.clear = function(){
+      temp = StorageService.load(); //assign LocalStorage data before clearing all tasks
+      items.splice(0,items.length);
+      StorageService.save(items); //after clearing all tasks, save to LocalStorage
+	  };
+    
+    service.undo = function(){		
+	    items = temp;  //assign items to the previous data stored in temp
+		  StorageService.save(items); //save to LocalStorage to update
+	  };
+    
+    service.getItems = function(){
+		  return items; 
+	  };
+  }
 
-	  $scope.addItem = function(){
-	    $scope.errorText = "";
-	    if(!$scope.addMe){ return; }
-	    if($scope.products.indexOf($scope.addMe) === -1 ){ //checks index of addMe (-1 if not in array)
-        $scope.products.push($scope.addMe);
-        if (typeof(Storage) !== "undefined") {
-          localStorage.setItem("tasks", JSON.stringify($scope.products));
-        } 
-	      $scope.addMe = "";
-	    }
-	    else{
-		    $scope.errorText = "Item already in the list";
-		    $scope.addMe = "";
-	    }
-	  }
-	  $scope.removeItem = function(x){
-	    $scope.errorText = "";
-	    $scope.products.splice(x,1);
-      if (typeof(Storage) !== "undefined") {
-        localStorage.setItem("tasks", JSON.stringify($scope.products));
-      } 
-	  }
-    $scope.removeAll = function(){
-      $scope.products = [];
-      if (typeof(Storage) !== "undefined") {
-        localStorage.setItem("tasks", JSON.stringify($scope.products));
-      } 
-    }
-  });
+//Factory - for TaskService
+	function TaskFactory(StorageService){
+	  var factory = function(){
+		  return new TaskService(StorageService);  
+	  };
+	  return factory;
+	}
   
 })(); 
 
